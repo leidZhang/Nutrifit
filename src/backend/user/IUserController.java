650d756163a8f095ae7246170c292603e2cd2812
LeidZhang <haoz28889@gmail.com>
2023-10-18 23:01:25 -0400

    reconstruct directory

--- src/backend/user/IUserController.java
+++ src/backend/user/IUserController.java
@@ -3,7 +3,7 @@ package backend.user;
 import backend.common.Result;
 import backend.user.entity.User;
 
-public interface IUser {
+public interface IUserController {
     Result save(User user);
     Result updateUser(String username);
     Result getUser(String username);
