84dbff3856079bec65167a5f6a131a14fc284fc3
dyliu <liudy2021@outlook.com>
2023-11-17 15:58:07 -0500

    add exercise data class to receive four exercise data from frontend

---
+++ src/main/backend/exercise/ExerciseData.java
@@ -0,0 +1,25 @@
+package main.backend.exercise;
+
+import java.sql.Date;
+
+public class ExerciseData {
+    private final Date date;
+    private final String type;
+    private final String intensity;
+    private final int duration;
+
+    public ExerciseData(Date date, String type, String intensity, int duration) {
+        this.date = date;
+        this.type = type;
+        this.intensity = intensity;
+        this.duration = duration;
+    }
+
+    public Date getDate() { return date; }
+
+    public String getType() { return type; }
+
+    public String getIntensity() { return intensity; }
+
+    public int getDuration() { return duration; }
+}
