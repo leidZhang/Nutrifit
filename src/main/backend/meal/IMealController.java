730dfec309f6365d556146144d9d94b66584c617
WeiyeMao <113457733+WeiyeMao@users.noreply.github.com>
2023-11-17 07:50:02 -0500

    Update IMealController.java

--- src/main/backend/meal/IMealController.java
+++ src/main/backend/meal/IMealController.java
@@ -1,4 +1,16 @@
 package main.backend.meal;
 
+import main.backend.common.Result;
+import main.backend.meal.entity.Meal;
+import main.backend.user.entity.User;
+
+import javax.xml.ws.Response;
+import java.util.Date;
+import java.util.List;
+
 public interface IMealController {
+    Result save(Meal meal, User user);
+    Result delete(int id);
+    Result getByUser(User user);
+    Result getByPeriod(User user, Date startDate, Date endDate);
 }
