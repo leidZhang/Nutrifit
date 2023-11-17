d4e354e049f5adb2d30881855e3e4e96085efc14
WeiyeMao <113457733+WeiyeMao@users.noreply.github.com>
2023-11-17 07:51:46 -0500

    Update IMealService.java

--- src/main/backend/meal/IMealService.java
+++ src/main/backend/meal/IMealService.java
@@ -1,7 +1,15 @@
 package main.backend.meal;
 
 import main.backend.meal.entity.Meal;
+import main.backend.user.entity.User;
+
+import java.sql.SQLException;
+import java.util.Date;
+import java.util.List;
 
 public interface IMealService {
-    void save(Meal meal);
+    void save(Meal meal, User user) throws SQLException;
+    void delete(int id) throws SQLException;
+    List<Meal> getByUser(User user) throws SQLException;
+    List<Meal> getByPeriod(User user, Date startDate, Date endDate) throws SQLException;
 }
