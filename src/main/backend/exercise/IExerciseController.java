8281e0b6434a4a0c5d82d2346426d4b8d5466362
dyliu <liudy2021@outlook.com>
2023-11-17 15:57:04 -0500

    update exercise controller and exercise service

--- src/main/backend/exercise/IExerciseController.java
+++ src/main/backend/exercise/IExerciseController.java
@@ -1,4 +1,16 @@
 package main.backend.exercise;
 
+import main.backend.common.Result;
+import main.backend.exercise.entity.Exercise;
+import main.backend.user.entity.User;
+
+import java.sql.Date;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface IExerciseController {
+    Result save(Exercise history, User user);
+    Result delete(int id);
+    Result getByUsername(String name);
+    Result getByPeriod(String name, Date start, Date end);
 }
--- src/main/backend/exercise/impl/ExerciseController.java
+++ src/main/backend/exercise/impl/ExerciseController.java
@@ -1,6 +1,65 @@
 package main.backend.exercise.impl;
 
+import main.backend.exercise.ExerciseData;
 import main.backend.exercise.IExerciseController;
+import main.backend.exercise.IExerciseService;
+import main.backend.exercise.entity.Exercise;
+import main.backend.user.entity.User;
+import main.backend.common.Result;
+
+import java.sql.Date;
+import java.util.List;
+
 
 public class ExerciseController implements IExerciseController {
+    private IExerciseService exerciseService;
+
+    public ExerciseController(IExerciseService exerciseService) {
+        this.exerciseService = exerciseService;
+    }
+
+    public void saveExerciseData(ExerciseData data, User user) {
+        exerciseService.saveExerciseData(data, user);
+    }
+
+    @Override
+    public Result save(Exercise exercise, User user) {
+        try {
+            exerciseService.save(exercise, user);
+            return Result.success("Exercise saved successfully!");
+        }catch(Exception e) {
+            return Result.error("Error saving exercise: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Result delete(int id) {
+        try {
+            exerciseService.delete(id);
+            return Result.success("Exercise deleted successfully!");
+        }catch(Exception e) {
+            return Result.error("Error deleting exercise: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Result getByUsername(String username) {
+        try {
+            List<Exercise> exercises = exerciseService.getByUsername(username);
+            return Result.success(exercises);
+        } catch (Exception e) {
+            return Result.error("Error retrieving exercises: " + e.getMessage());
+        }
+    }
+
+    @Override
+    public Result getByPeriod(String username, Date start, Date end) {
+        try {
+            List<Exercise> exercises = exerciseService.getByPeriod(username, start, end);
+            return Result.success(exercises);
+        } catch (Exception e) {
+            return Result.error("Error retrieving exercises by period: " + e.getMessage());
+        }
+    }
 }
+
